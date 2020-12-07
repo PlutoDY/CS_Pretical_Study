d231871a8d818cda5dec345345be0c4a576252ac
PlutoDY <dj157314@gmail.com>
2020-12-07 13:29:59 +0900

    Add_function
    Meter to feet기능 추가

--- 01_Distance_Converter/01_Distance converter.cs
+++ 01_Distance_Converter/01_Distance converter.cs
@@ -10,17 +10,36 @@ namespace Practical_CS_StudyProject._01_Distance_Converter
     {
         private static void Main(string[] args)
         {
-            // feet -> meter
-            for(int feet = 1; feet <= 10; feet++)
+           // 인수 지정 -> Length가 1이상이고 인수가 tom이면 Feet -> Meter
+            if (args.Length >= 1 && args[0] == "-tom")
             {
-                double meter = FeetToMeter(feet);
-                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+                for (int feet = 1; feet <= 10; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+                }
+            }
+            // 그 이외일 시 Meter -> Feet
+            else
+            {
+                for(int meter = 1; meter <= 10; meter++)
+                {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+                }
             }
         }
 
+
+        // 계산 로직을 메서드의 형태로 독립
         private static double FeetToMeter(int feet)
         {
             return feet * 0.3048;
+        }
+
+        private static double MeterToFeet(int meter)
+        {
+            return meter / 0.3048;
         }
     }
 }
