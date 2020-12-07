578c5324e16427582daf9c7401a876140c7655e2
PlutoDY <dj157314@gmail.com>
2020-12-07 13:11:06 +0900

    Create 01_Distance converter.cs
    Distance converter 1st_Ver.

---
+++ 01_Distance converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_CS_StudyProject._01_Distance_Converter
+{
+    class _01_Distance_converter
+    {
+        private static void Main(string[] args)
+        {
+            // feet -> meter
+            for(int feet = 1; feet <= 10; feet++)
+            {
+                double meter = feet * 0.3848;
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+    }
+}
